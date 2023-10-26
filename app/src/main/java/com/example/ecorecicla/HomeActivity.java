420eb9be9d1f18f728b5c31a3636ac8d7cc46e1f
BrayanStic <brayan.fontecha.ruiz@gmail.com>
2023-10-26 15:40:39 -0500

    Cuarto commit

--- app/src/main/java/com/example/ecorecicla/HomeActivity.java
+++ app/src/main/java/com/example/ecorecicla/HomeActivity.java
@@ -1,14 +1,64 @@
 package com.example.ecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.utils.widget.ImageFilterView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button categ;
+    Button estd;
+    Button conj;
+
+    ImageFilterView exit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        categ=findViewById(R.id.buttonCateg);
+        estd=findViewById(R.id.buttonEst);
+        conj=findViewById(R.id.buttonConj);
+        exit=findViewById(R.id.imageViewExit);
+
+        Intent categoria=new Intent(getApplicationContext(), CategoriaActivity.class);
+        Intent estadist=new Intent(getApplicationContext(), EstadisticaActivity.class);
+        Intent conjo=new Intent(getApplicationContext(),ConsejosActivity.class);
+        Intent salir=new Intent(getApplicationContext(),MainActivity.class);
+
+        categ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(categoria);
+            }
+        });
+
+        estd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(estadist);
+            }
+        });
+
+        conj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(conjo);
+            }
+        });
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(salir);
+            }
+        });
+
     }
 }
