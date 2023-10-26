415140e8a5fe580c2655859d12fdeb719e15315d
BrayanStic <brayan.fontecha.ruiz@gmail.com>
2023-10-25 20:06:06 -0500

    segundo commit

--- app/src/main/java/com/example/ecorecicla/EstadisticaActivity.java
+++ app/src/main/java/com/example/ecorecicla/EstadisticaActivity.java
@@ -2,13 +2,41 @@ package com.example.ecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class EstadisticaActivity extends AppCompatActivity {
+
+    ImageView home;
+    ImageView atras;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadistica);
+
+        home=findViewById(R.id.imageHome);
+        atras=findViewById(R.id.imageFlechaAtras);
+
+        Intent pantallaPrin=new Intent(getApplicationContext(), HomeActivity.class);
+        Intent pantallaAtras=new Intent(getApplicationContext(), HomeActivity.class);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(pantallaPrin);
+            }
+        });
+
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(pantallaAtras);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/ecorecicla/MainActivity.java
+++ app/src/main/java/com/example/ecorecicla/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.ecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button ingresar;
+    TextView recuperarCont;
+    TextView registrarUsuario;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ingresar=findViewById(R.id.buttonIngresar);
+        recuperarCont=findViewById(R.id.textViewOlvCont);
+        registrarUsuario=findViewById(R.id.textViewNuevoRegist);
+
+        Intent ingresa=new Intent(getApplicationContext(), HomeActivity.class);
+        Intent olvidarCont=new Intent (getApplicationContext(), RecuperarContrasenaActivity.class);
+        Intent registrarUser=new Intent (getApplicationContext(), RegistroUsuarioActivity.class);
+
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(ingresa);
+            }
+        });
+
+        recuperarCont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(olvidarCont);
+            }
+        });
+
+        registrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(registrarUser);
+            }
+        });
     }
 }
