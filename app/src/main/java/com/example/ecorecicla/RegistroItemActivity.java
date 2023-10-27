5c7d09db94e5736f0c96f115b7231dccb4ed8b5f
BrayanStic <brayan.fontecha.ruiz@gmail.com>
2023-10-27 06:43:21 -0500

    noveno commit

--- app/src/main/java/com/example/ecorecicla/RegistroItemActivity.java
+++ app/src/main/java/com/example/ecorecicla/RegistroItemActivity.java
@@ -2,14 +2,50 @@ package com.example.ecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class RegistroItemActivity extends AppCompatActivity {
+
+    Button regis;
+    ImageView home,atras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_item);
+
+        regis=findViewById(R.id.buttonRegItem);
+        home=findViewById(R.id.logoHome);
+        atras=findViewById(R.id.atrasFlecha);
+
+        Intent pantallaEstd= new Intent(getApplicationContext(), EstadisticaActivity.class);
+        Intent pantallaPrin=new Intent(getApplicationContext(), HomeActivity.class);
+        Intent pantallaAtras=new Intent(getApplicationContext(), HomeActivity.class);
+
+        regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(pantallaEstd);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(pantallaPrin);
+            }
+        });
+
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(pantallaAtras);
+            }
+        });
+
 
 
     }
