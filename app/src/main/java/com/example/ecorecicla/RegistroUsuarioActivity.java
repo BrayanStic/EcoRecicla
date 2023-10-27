5b3ba694b27f4cdb278582185f586652e96af02c
BrayanStic <brayan.fontecha.ruiz@gmail.com>
2023-10-26 21:33:46 -0500

    octavo commit

--- app/src/main/java/com/example/ecorecicla/RegistroUsuarioActivity.java
+++ app/src/main/java/com/example/ecorecicla/RegistroUsuarioActivity.java
@@ -2,13 +2,29 @@ package com.example.ecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistroUsuarioActivity extends AppCompatActivity {
+
+    Button newUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
+        newUsuario=findViewById(R.id.buttonRegUsuario);
+
+        Intent newUser=new Intent(getApplicationContext(), HomeActivity.class);
+
+        newUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(newUser);
+            }
+        });
     }
 }
