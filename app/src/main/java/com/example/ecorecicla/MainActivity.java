f181e061ea4a1fafe311d9e757b655b4b1cb9504
BrayanStic <brayan.fontecha.ruiz@gmail.com>
2023-11-29 15:39:43 -0500

    registro y login User

--- app/src/main/java/com/example/ecorecicla/MainActivity.java
+++ app/src/main/java/com/example/ecorecicla/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.ecorecicla;
 
+import static java.lang.Thread.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ecorecicla.models.User;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button ingresar;
+    Button login;
     TextView recuperarCont;
-    TextView registrarUsuario;
+    TextView register;
+
+    EditText user, password;
 
 
     @Override
@@ -20,20 +33,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ingresar=findViewById(R.id.buttonIngresar);
+        login=findViewById(R.id.buttonIngresar);
         recuperarCont=findViewById(R.id.textViewOlvCont);
-        registrarUsuario=findViewById(R.id.textViewNuevoRegist);
+        register=findViewById(R.id.textViewNuevoRegist);
+        user=findViewById(R.id.editTextUsuario);
+        password=findViewById(R.id.editTextContraseña);
 
-        Intent ingresa=new Intent(getApplicationContext(),HomeActivity.class);
+        File fileReader = new File(getFilesDir(),"user.txt");
+
+        ArrayList<User> usersList= listUsers(fileReader);
+
         Intent olvidarCont=new Intent (getApplicationContext(), RecuperarContrasenaActivity.class);
-        Intent registrarUser=new Intent (getApplicationContext(), RegistroUsuarioActivity.class);
+        Intent registerUser=new Intent (getApplicationContext(), RegistroUsuarioActivity.class);
+        Intent home= new Intent(getApplicationContext(), HomeActivity.class);
 
-        ingresar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(ingresa);
-            }
-        });
 
         recuperarCont.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,11 +55,81 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        registrarUsuario.setOnClickListener(new View.OnClickListener() {
+
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(registrarUser);
+                boolean state= false;
+                if (user.getText().toString().isEmpty() ||
+                        password.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Ambos campos deben estar completos",
+                            Toast.LENGTH_LONG).show();
+                }else{
+                    for(User i:usersList){
+                        if (i.getName().equals(user.getText().toString())||
+                                i.getEmail().equals(user.getText().toString())||
+                                i.getPhone().equals(user.getText().toString())){
+                            state=true;
+                            if(i.getPassword().equals(password.getText().toString())){
+
+                                home.putExtra("idUser",i.getIdUser());
+
+                                Toast.makeText(getApplicationContext(),
+                                        "Los datos son correcto",Toast.LENGTH_LONG).show();
+                                try {
+                                    sleep(500);
+                                } catch (InterruptedException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                startActivity(home);
+                                break;
+                            }else {
+                                Toast.makeText(getApplicationContext(),
+                                        "La contraseña es incorrecta",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                    if (!state){
+                        Toast.makeText(getApplicationContext(),
+                                "Este usuario no esta registrado",Toast.LENGTH_LONG).show();
+                    }
+
+                }
+
             }
         });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(registerUser);
+            }
+        });
+    }
+
+    public ArrayList<User> listUsers(File fileUser){
+        ArrayList<User> list= new ArrayList<>();
+        try {
+            FileReader fileReader= new FileReader(fileUser);
+            BufferedReader reader= new BufferedReader(fileReader);
+            String user;
+
+            while ((user=reader.readLine())!=null){
+                String[] userArray = user.split(",");
+                String id= userArray[0];
+                String name= userArray[1];
+                String email= userArray[2];
+                String phone= userArray[3];
+                String password= userArray[4];
+
+                User userObj= new User(id,name,email,phone,password);
+                list.add(userObj);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return list;
+
     }
 }
--- app/src/main/java/com/example/ecorecicla/RegistroUsuarioActivity.java
+++ app/src/main/java/com/example/ecorecicla/RegistroUsuarioActivity.java
@@ -1,16 +1,29 @@
 package com.example.ecorecicla;
 
+import static java.lang.Thread.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.ecorecicla.models.User;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Random;
 
 public class RegistroUsuarioActivity extends AppCompatActivity {
 
-    Button newUsuario;
+    Button register;
+    EditText name, email, phone, password1,password2;
     ImageView atras;
 
     @Override
@@ -19,22 +32,120 @@ public class RegistroUsuarioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registro_usuario);
 
         atras=findViewById(R.id.atrasFlecha);
-        newUsuario=findViewById(R.id.buttonRegUsuario);
+        register=findViewById(R.id.buttonRegUsuario);
+        name=findViewById(R.id.name_user);
+        email=findViewById(R.id.email_user);
+        phone=findViewById(R.id.phone_user);
+        password1=findViewById(R.id.password1_user);
+        password2=findViewById(R.id.password2_user);
 
         Intent flechaAtras=new Intent(getApplicationContext(),MainActivity.class);
-        Intent newUser=new Intent(getApplicationContext(), HomeActivity.class);
-
+        Intent home=new Intent(getApplicationContext(), HomeActivity.class);
         atras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(flechaAtras);
             }
         });
-        newUsuario.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(newUser);
+            public void onClick(View v) {
+                if (validateUser()){
+                    User user=createUser();
+                    storageUser(user);
+                    Toast.makeText(getApplicationContext(),"Registro Exitoso"
+                            ,Toast.LENGTH_LONG).show();
+                    try {
+                        sleep(500);
+                        startActivity(home);
+                        finish();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }else{
+                    Toast.makeText(getApplicationContext(),
+                            "Asegurarse de que la información esta correcta",Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+
+    public String idGenerator(String name){
+        String id="";
+        for (int i=0; i<2;i++){
+            int random1= (int)(Math.random()*name.length());
+            int random2= (int)(Math.random()*10000);
+            id+=name.charAt(random1);
+            id+=random2;
+        }
+        return id;
+    }
+    public boolean validateUser(){
+        boolean state= true;
+
+        if (name.getText().toString().isEmpty()){
+            name.setBackgroundColor(Color.RED);
+            state=false;
+        }
+        if (email.getText().toString().isEmpty()){
+            email.setBackgroundColor(Color.RED);
+            state=false;
+        }
+        if (phone.getText().toString().isEmpty()){
+            phone.setBackgroundColor(Color.RED);
+            state=false;
+        }
+        if (password1.getText().toString().isEmpty()){
+            password1.setBackgroundColor(Color.RED);
+            state=false;
+        }
+        if (password2.getText().toString().isEmpty()){
+            password2.setBackgroundColor(Color.RED);
+            state=false;
+        }
+        if (!password1.getText().toString().equals(
+                password2.getText().toString())){
+            password1.setBackgroundColor(Color.RED);
+            password2.setBackgroundColor(Color.RED);
+            state=false;
+        }
+
+        return state;
+    }
+
+    public User createUser(){
+        String name_user=name.getText().toString();
+        String id=idGenerator(name_user);
+        String email_user=email.getText().toString();
+        String phone_user=phone.getText().toString();
+        String password= password1.getText().toString();
+
+        User user= new User(id,name_user,email_user,phone_user,password);
+
+        return user;
+    }
+    public void storageUser(User user){
+
+
+        File fileUser= new File(getFilesDir(),"user.txt");
+
+        try {
+            FileWriter writer= new FileWriter(fileUser,true);
+
+                 BufferedWriter bufferedWriter=new BufferedWriter(writer);
+            bufferedWriter.write( user.getIdUser()+","+
+                    user.getName()+","+
+                    user.getEmail()+","+
+                    user.getPhone()+","+
+                    user.getPassword()
+            );
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+        }catch (Exception error){
+            error.printStackTrace();
+        }
+
     }
 }
